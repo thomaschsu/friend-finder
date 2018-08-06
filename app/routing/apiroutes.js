7b906adfb3291d4178f05eec561dff954b487f23
Tom Hsu <thomaschsu@gmail.com>
2018-08-05 23:33:43 -0400

    Fixed friends and apiroutes

---
+++ app/routing/apiroutes.js
@@ -0,0 +1,15 @@
+// Requires friends js file
+var friends = require("../data/friends");
+
+
+module.exports = function(app) {
+  // API GET Requests
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  app.post("/api/friends", function(req, res) {
+      friends.push(req.body);
+      res.json(true);
+  });
+};
